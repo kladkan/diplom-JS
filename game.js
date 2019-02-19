380bed974b71b62c193e07c117b56b6fb93d335d
kladkan <43966184+kladkan@users.noreply.github.com>
2019-02-19 12:39:45 +0300

    Убрал отступы после 3-й строки. Дописал клас Level

--- game.js
+++ game.js
@@ -1,120 +1,195 @@
 'use strict';
 class Vector {
-    constructor(x = 0, y = 0) {
-      this.x = x;
-      this.y = y;
-    }
+  constructor(x = 0, y = 0) {
+    this.x = x;
+    this.y = y;
+  }
+
+  plus(newVector) {
+    if (!(newVector instanceof Vector)) {
+      throw new Error('Можно прибавлять к вектору только вектор класса Vector');
+    };
   
-    plus(newVector) {
-      if (!(newVector instanceof Vector)) {
-        throw new Error('Можно прибавлять к вектору только вектор типа Vector');
-      };
+    return new Vector(
+      this.x + newVector.x,
+      this.y + newVector.y
+    );
+  }
+  times(multiplier) {
+    return new Vector(
+      this.x * multiplier,
+      this.y * multiplier
+    );
+  }
+}
   
-      return new Vector(
-        this.x + newVector.x,
-        this.y + newVector.y
-      );
-    }
-  
-    times(multiplier) {
-      return new Vector(
-        this.x * multiplier,
-        this.y * multiplier
-      );
-    }
+//----------------------------
+class Actor {
+  constructor(pos = new Vector(0, 0), size = new Vector(1, 1), speed = new Vector(0, 0)) {
+    if (!(pos instanceof Vector) || !(size instanceof Vector) || !(speed instanceof Vector)) {
+      throw new Error('Атрибуты движущегося объекта должны быть объектами класса Vector');
+    };
+    this.pos = pos;
+    this.size = size;
+    this.speed = speed;  
   }
   
-  //----------------------------
-  class Actor {
-    constructor(pos = new Vector(0, 0), size = new Vector(1, 1), speed = new Vector(0, 0)) {
-      if (!(pos instanceof Vector) || !(size instanceof Vector) || !(speed instanceof Vector)) {
-        throw new Error('Атрибуты движущегося объекта должны быть объектами типа Vector');
-      };
-      this.pos = pos;
-      this.size = size;
-      this.speed = speed;  
-    }
+  act() {
+  }
+  get left() {
+    return this.pos.x;
+  }  
+  get top() {
+    return this.pos.y;
+  }
+  get right() {
+    return this.pos.x + this.size.x;
+  }
+  get bottom() {
+    return this.pos.y + this.size.y;
+  }
+  get type() {
+    return 'actor';
+  }
   
-    act() {
-    }
-    get left() {
-      return this.pos.x;
-    }  
-    get top() {
-      return this.pos.y;
-    }
-    get right() {
-      return this.pos.x + this.size.x;
-    }
-    get bottom() {
-      return this.pos.y + this.size.y;
-    }
-    get type() {
-      return 'actor';
-    }
-  
-    isIntersect(newActor) {
-      if (newActor === undefined) {
-        throw new Error('Метод <isIntersect> не может быть вызван без аргумента');
-      } 
-      if (!(newActor instanceof Actor)) {
-        throw new Error('В качестве аргумента может быть только объект типа Actor');
-      };
+  isIntersect(newActor) {
+    if (newActor === undefined) {
+      throw new Error('Метод <isIntersect> не может быть вызван без аргумента');
+    } 
+    if (!(newActor instanceof Actor)) {
+      throw new Error('В качестве аргумента может быть только объект класса Actor');
+    };
 
-      if (newActor.bottom <= this.top || newActor.left >= this.right || newActor.top >= this.bottom || newActor.right <= this.left) {
+    if (newActor.bottom <= this.top || newActor.left >= this.right || newActor.top >= this.bottom || newActor.right <= this.left) {
         return false
-      }
+    }
 
-      if (this === newActor) {
-        return false
-      } 
+    if (this === newActor) {
+      return false
+    } 
+    return true;
+  }
+}
+
+//---------------------------------
+class Level {
+  constructor(grid, actors) {
+    this.grid = grid;
+    this.actors = actors;
+    this.player = actors.find(player => player.type === 'player');
+    this.height = grid.length;
+    this.width = Math.max(...grid.map(el => el.length));
+    this.status = null;
+    this.finishDelay = 1;
+  }
+
+  isFinished() {
+    if (this.status !== null && this.finishDelay < 0) {
       return true;
     }
   }
 
-  //---------------------------------
-  class Level {
-    constructor(grid, actors) {
-      this.grid = grid;
-      this.actors = actors;
-      this.player = actors.find(player => player.type === 'player');
-      this.height = grid.length;
-      this.width = Math.max(...grid.map(el => el.length));
-      this.status = null;
-      this.finishDelay = 1;
+  actorAt(newActor) {
+    if (newActor === undefined) {
+      throw new Error('Метод <actorAt> не может быть вызван без аргумента');
+    } 
+    if (!(newActor instanceof Actor)) {
+      throw new Error('В качестве аргумента может быть только объект класса Actor');
     }
 
-    isFinished() {
-      if (this.status !== null && this.finishDelay < 0) {
-        return true;
-      }
-    }
-
-    actorAt(actor) {
-      if (actor === undefined) {
-        throw new Error('Метод <actorAt> не может быть вызван без аргумента');
-      } 
-      if (!(actor instanceof Actor)) {
-        throw new Error('В качестве аргумента может быть только объект типа Actor');
-      }
-      if (actor.pos === this.pos) {
+    for (let actor of this.actors) {
+      if (actor.pos === newActor.pos) {
         return actor;
       }
-      
-
     }
 
-
+    for (let actor of this.actors) {
+      if (newActor.bottom <= actor.top || newActor.left >= actor.right || newActor.top >= actor.bottom || newActor.right <= actor.left) {
+        return actor;
+      }
+      return undefined;
+    }  
   }
+
+  obstacleAt(newPos, objSize) {
+    if (!(newPos instanceof Vector) || !(objSize instanceof Vector)) {
+      throw new Error('В качестве аргументов может быть только объект класса Actor');
+    };
+
+    if (newPos.x > 0) {
+      if (objSize.right + newPos.x > this.width) {
+        return 'wall';
+      }
+      if (this.grid[objSize.pos.y][objSize.right + newPos.x] === undefined) {
+        return undefined;
+      } else {
+        return this.grid[objSize.pos.y][objSize.right + newPos.x];
+      }
+    }
+
+    if (newPos.x < 0) {
+      if (objSize.left - newPos.x < 0) {
+        return 'wall';
+      }
+      if (this.grid[objSize.pos.y][objSize.left - newPos.x] === undefined) {
+        return undefined;
+      } else {
+        return this.grid[objSize.pos.y][objSize.left - newPos.x];
+      }
+    }
+
+    if (newPos.y > 0) {
+      if (objSize.bottom + newPos.y > this.height) {
+        return 'lava';
+      }
+      if (this.grid[objSize.bottom + newPos.y][objSize.pos.x] === undefined) {
+        return undefined;
+      } else {
+        return this.grid[objSize.bottom + newPos.y][objSize.pos.x];
+      }
+    }
+
+    if (newPos.y < 0) {
+      if (objSize.top - newPos.y < 0) {
+        return 'wall';
+      }
+      if (this.grid[objSize.top - newPos.y][objSize.pos.x] === undefined) {
+        return undefined;
+      } else {
+        return this.grid[objSize.top - newPos.y][objSize.pos.x];
+      }
+    }
+  }
+
+  removeActor(delActor) {
+    this.actors = this.actors.filter(actor => actor !== delActor);
+  }
+
+  noMoreActors(type) {
+    if (this.actors.find(actor => actor.type === type) === undefined) {
+      return true;
+    }
+    return false;
+  }
+
+
+}
 /*
-Метод actorAt
-Определяет, расположен ли какой-то другой движущийся объект в переданной позиции, и если да, вернёт этот объект.
+const myArray = [
+{name: 'Bill', age: 15},
+{name: 'Max', age: 40},
+{name: 'Casey', age: 23},
+]
+const oldMan = myArray.find(function (man) {
+return man.age > 50;
+})
+console.log(oldMan); // => undefined
+Метод noMoreActors
+Определяет, остались ли еще объекты переданного типа на игровом поле.
 
-Принимает один аргумент — движущийся объект, Actor. Если не передать аргумент или передать не объект Actor, метод должен бросить исключение.
+Принимает один аргумент — тип движущегося объекта, строка.
 
-Возвращает undefined, если переданный движущийся объект не пересекается ни с одним объектом на игровом поле.
-
-Возвращает объект Actor, если переданный объект пересекается с ним на игровом поле. Если пересекается с несколькими объектами, вернет первый.
+Возвращает true, если на игровом поле нет объектов этого типа (свойство type). Иначе возвращает false.
 */
 //Пример кода
 const grid = [
